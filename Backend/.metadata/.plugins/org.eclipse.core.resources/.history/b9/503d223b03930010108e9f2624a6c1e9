0fec1f11a83532923b1835569755e70860a0bb20
raj100aseel <raj100aseel@gmail.com>
2025-09-16 19:46:44 +0530

    mini project updated

---
+++ Backend/.metadata/.plugins/org.eclipse.core.resources/.history/b9/503d223b03930010108e9f2624a6c1e9
@@ -0,0 +1,23 @@
+package com.rajtech.empmgt.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rajtech.empmgt.entity.Employee;
+import com.rajtech.empmgt.repository.EmployeeRepository;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+public class EmployeeController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")//http://localhost:8080/employees
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+}
--- Backend/EmployeeManagementSystem/src/main/java/com/rajtech/empmgt/controller/EmployeeController.java
+++ Backend/EmployeeManagementSystem/src/main/java/com/rajtech/empmgt/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rajtech.empmgt.entity.Employee;
@@ -20,4 +22,26 @@ public class EmployeeController {
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
